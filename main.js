1e6f249a43cbe380fe0c623935c520bf4bead83d
AbnerAraujo1 <105257875+AbnerAraujo1@users.noreply.github.com>
2022-08-29 22:45:56 -0300

    correção bug scroll

--- main.js
+++ main.js
@@ -64,8 +64,16 @@ const scrollReveal =ScrollReveal({
 })
 
 scrollReveal.reveal(
-`#home .text, #home .image, 
-`)
+    `#home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials
+    #contact .text, #contact .links,
+    footer .brand, footer .social
+    `,
+    { interval: 100 }
+  )
+
 
 
 
